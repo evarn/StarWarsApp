99f6cb52de7305cfbdf43e8be3146b817b2b6d93
Eugene Varnashin <nevadaru@yandex.ru>
2023-03-02 23:08:16 +0300

    feat: add check color

--- src/screens/card/components/CardCharacter.tsx
+++ src/screens/card/components/CardCharacter.tsx
@@ -1,11 +1,5 @@
 import React from 'react';
-import {
-  StyleSheet,
-  View,
-  Text,
-  TouchableOpacity,
-  Dimensions,
-} from 'react-native';
+import {StyleSheet, View, Text, TouchableOpacity} from 'react-native';
 import Fonts from '../../../constants/fonts';
 import Colors from '../../../constants/colors';
 import CircleCharacter from './Circle';
@@ -32,17 +26,53 @@ interface ICardCharacter {
   widthSIZE?: string | number | undefined;
 }
 const CardCharacter = ({item, widthSIZE}: ICardCharacter) => {
-  const {name, mass, height} = item;
+  const {name, mass, height, birth_year, gender} = item;
+
+  const getCheckColor = (check: string) => {
+    if (check === 'none') {
+      return;
+    }
+    if (check === 'unknown') {
+      return;
+    }
+    if (check === 'n/a') {
+      return;
+    }
+    return true;
+  };
 
   return (
-    <TouchableOpacity
-      style={[styles.mainContainer, {margin: 10, width: widthSIZE}]}>
+    <TouchableOpacity style={[styles.mainContainer, {width: widthSIZE}]}>
       <Text style={styles.name}>{name}</Text>
       <View style={styles.circleContainer}>
         {height !== 'unknown' && (
           <CircleCharacter title={'height'} param={height} />
         )}
         {mass !== 'unknown' && <CircleCharacter title={'mass'} param={mass} />}
+      </View>
+      <View style={styles.viewCharcontainer}>
+        {getCheckColor(birth_year) && (
+          <View style={[styles.viewChar, {backgroundColor: Colors.BLUE_1}]}>
+            <Text style={styles.textChar}>{birth_year} </Text>
+          </View>
+        )}
+
+        {getCheckColor(gender) && (
+          <View
+            style={[
+              styles.viewChar,
+              {
+                backgroundColor:
+                  gender === 'male'
+                    ? Colors.GREEN_1
+                    : gender === 'female'
+                    ? Colors.VIOLET_1
+                    : Colors.YELLOW_1,
+              },
+            ]}>
+            <Text style={styles.textChar}>{gender} </Text>
+          </View>
+        )}
       </View>
     </TouchableOpacity>
   );
@@ -57,6 +87,7 @@ const styles = StyleSheet.create({
     paddingHorizontal: 20,
     paddingVertical: 8,
     borderRadius: 8,
+    margin: 10,
   },
   name: {
     fontFamily: Fonts.PRIMARY,
@@ -65,11 +96,28 @@ const styles = StyleSheet.create({
     fontSize: 18,
     color: Colors.BLACK_2,
     textAlign: 'center',
-
     height: 46,
   },
   circleContainer: {
     flexDirection: 'row',
+  },
+  viewCharcontainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+  },
+  viewChar: {
+    marginHorizontal: 4,
+    padding: 8,
+    borderRadius: 11,
+  },
+  textChar: {
+    fontFamily: Fonts.SECONDARY,
+    fontStyle: 'normal',
+    fontWeight: '400',
+    fontSize: 12,
+    lineHeight: 14,
+    textAlign: 'center',
+    color: Colors.BLACK_2,
   },
 });
 
